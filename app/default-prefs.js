c6177d32ce5a1922fb6ad3eb7ec4f09d82e02263
Dave Townsend <dtownsend@oxymoronical.com>
2007-09-13 16:52:42 +0000

    Bug 394826: Initial McCoy landing. r=mfinkle

---
+++ app/default-prefs.js
@@ -0,0 +1,8 @@
+pref("toolkit.defaultChromeURI", "chrome://mccoy/content");
+pref("toolkit.defaultChromeFeatures", "chrome,resizable,centerscreen,dialog=no");
+pref("network.protocol-handler.expose-all", false);
+pref("app.update.enabled", false);
+
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("browser.dom.window.dump.enabled", true);
