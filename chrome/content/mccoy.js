3a2b3bd1f101f6355bc1b1f893b9bfd7acbbd209
Dave Townsend <dtownsend@oxymoronical.com>
2008-06-02 16:53:59 +0000

    Bug 435858: Add auto update support. r=nthomas

--- chrome/content/mccoy.js
+++ chrome/content/mccoy.js
@@ -55,6 +55,56 @@ function openWindowForType(type, uri, features) {
     window.open(uri, "_blank", "chrome,extrachrome,menubar,resizable,scrollbars,status,toolbar");
 }
 
+function buildHelpMenu()
+{
+  var updates = Cc["@mozilla.org/updates/update-service;1"].
+                getService(Ci.nsIApplicationUpdateService);
+  var um = Cc["@mozilla.org/updates/update-manager;1"].
+           getService(Ci.nsIUpdateManager);
+
+  // Disable the UI if the update enabled pref has been locked by the 
+  // administrator or if we cannot update for some other reason
+  var checkForUpdates = document.getElementById("menu-update");
+  var canUpdate = updates.canUpdate;
+  checkForUpdates.setAttribute("disabled", !canUpdate);
+  if (!canUpdate)
+    return; 
+
+  var strings = document.getElementById("strings");
+  var activeUpdate = um.activeUpdate;
+  
+  // If there's an active update, substitute its name into the label
+  // we show for this item, otherwise display a generic label.
+  function getStringWithUpdateName(key) {
+    if (activeUpdate && activeUpdate.name)
+      return strings.getFormattedString(key, [activeUpdate.name]);
+    return strings.getString(key + "Fallback");
+  }
+  
+  // By default, show "Check for Updates..."
+  var key = "default";
+  if (activeUpdate) {
+    switch (activeUpdate.state) {
+    case "downloading":
+      // If we're downloading an update at present, show the text:
+      // "Downloading Firefox x.x..." otherwise we're paused, and show
+      // "Resume Downloading Firefox x.x..."
+      key = updates.isDownloading ? "downloading" : "resume";
+      break;
+    case "pending":
+      // If we're waiting for the user to restart, show: "Apply Downloaded
+      // Updates Now..."
+      key = "pending";
+      break;
+    }
+  }
+  checkForUpdates.label = getStringWithUpdateName("updatesItem_" + key);
+  if (um.activeUpdate && updates.isDownloading)
+    checkForUpdates.setAttribute("loading", "true");
+  else
+    checkForUpdates.removeAttribute("loading");
+}
+
 function openKeyManager() {
   var ks = Cc["@toolkit.mozilla.org/keyservice;1"].
            getService(Ci.nsIKeyService);
@@ -76,9 +126,32 @@ function openErrorConsole() {
   openWindowForType("global:console", "chrome://global/content/console.xul");
 }
 
+function openConfig() {
+  openWindowForType("Preferences:ConfigManager", "chrome://global/content/config.xul");
+}
+
 function openDOMInspector() {
   window.openDialog("chrome://inspector/content/", "_blank",
                     "chrome,all,dialog=no", document);
+}
+
+/**
+ * Opens the update manager and checks for updates to the application.
+ */
+function openUpdates()
+{
+  var um = Cc["@mozilla.org/updates/update-manager;1"].
+           getService(Ci.nsIUpdateManager);
+  var prompter = Cc["@mozilla.org/updates/update-prompt;1"].
+                 createInstance(Ci.nsIUpdatePrompt);
+
+  // If there's an update ready to be applied, show the "Update Downloaded"
+  // UI instead and let the user know they have to restart the browser for
+  // the changes to be applied. 
+  if (um.activeUpdate && um.activeUpdate.state == "pending")
+    prompter.showUpdateDownloaded(um.activeUpdate);
+  else
+    prompter.checkForUpdates();
 }
 
 function openAbout() {
